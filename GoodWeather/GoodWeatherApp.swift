aa02ef54e793e2073feba16680d69b552f439e63
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-28 18:55:55 +0900

    Initial Commit

---
+++ GoodWeather/GoodWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherApp.swift
+//  GoodWeather
+//
+//  Created by 飯塚政美 on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
